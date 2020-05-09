c4949f6b49f380cd6c6e48c2257d66604420ab91
jdh5563 <56982973+jdh5563@users.noreply.github.com>
2020-05-09 18:18:51 -0400

    Added basic player movement

---
+++ Project Capital A/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    //Field
+    public GameObject player;
+    private float xPos, yPos, zPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xPos = player.transform.position.x;
+        yPos = player.transform.position.y;
+        zPos = player.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            zPos += 0.1f;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            zPos -= 0.1f;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            xPos -= 0.1f;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            xPos += 0.1f;
+        }
+
+        player.transform.position = new Vector3(xPos, yPos, zPos);
+    }
+}
