507f3220770b693db936bd4bfa4f47af44d1b12d
jdh5563 <56982973+jdh5563@users.noreply.github.com>
2020-05-09 18:41:08 -0400

    Enhanced player movement
    The player's velocity is now changed instead of its position.

--- Project Capital A/Assets/Scripts/John Scripts/PlayerMove.cs
+++ Project Capital A/Assets/Scripts/John Scripts/PlayerMove.cs
@@ -5,38 +5,54 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     //Field
-    public GameObject player;
-    private float xPos, yPos, zPos;
+    public Rigidbody rb;
+    private float xVel, yVel, zVel;
 
     // Start is called before the first frame update
     void Start()
     {
-        xPos = player.transform.position.x;
-        yPos = player.transform.position.y;
-        zPos = player.transform.position.z;
+        xVel = rb.velocity.x;
+        yVel = rb.velocity.y;
+        zVel = rb.velocity.z;
     }
 
     // Update is called once per frame
     void Update()
     {
+        rb.velocity = Move();
+    }
+
+    private Vector3 Move()
+    {
+        yVel = rb.velocity.y;
         if (Input.GetKey(KeyCode.W))
         {
-            zPos += 0.1f;
+            zVel = 1;
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            zPos -= 0.1f;
+            zVel = -1;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            xPos -= 0.1f;
+            xVel = -1;
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            xPos += 0.1f;
+            xVel = 1;
         }
 
-        player.transform.position = new Vector3(xPos, yPos, zPos);
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
+        {
+            zVel = 0;
+        }
+
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            xVel = 0;
+        }
+
+        return new Vector3(xVel, yVel, zVel);
     }
 }
