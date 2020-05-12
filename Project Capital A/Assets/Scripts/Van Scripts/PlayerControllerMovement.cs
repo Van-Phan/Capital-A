d1f8e24cfd4bc3007458a9fe784503f2b5c4070d
Van Phan <infernosdaavenger@gmail.com>
2020-05-12 12:22:29 -0400

    Finished Player controller movement
    Added player controller movement script which moves the player using WASD with respect to the camera direction.

---
+++ Project Capital A/Assets/Scripts/Van Scripts/PlayerControllerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerMovement : MonoBehaviour
+{
+    //fields
+    public Transform camera;
+    public CharacterController player;
+
+    Vector2 input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //locks and hides cursor to middle of screen, press escape to enable mouse
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //determines which way the user wants to go based off WASD input
+        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        //normalizes our vector so if moving diagonally we don't move faster
+        input = input.normalized;
+
+        //retrieves the vectors of our camera
+        Vector3 cameraForward = camera.forward;
+        Vector3 cameraRight = camera.right;
+
+        //eliminates y vector since camera is rotated to look down slightly
+        cameraForward.y = 0;
+        cameraRight.y = 0;
+
+        //moves the player 
+        player.Move((cameraForward * input.y + cameraRight * input.x) * Time.deltaTime * 5);
+    }
+}
