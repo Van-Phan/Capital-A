2c563625637b237284ed7e1a7055184322c87fba
Van Phan <infernosdaavenger@gmail.com>
2020-05-11 22:00:42 -0400

    Changed movement
    Player can now strafe but the issue is they move bullet speed when strafing.

--- Project Capital A/Assets/Scripts/Van Scripts/PlayerMovement.cs
+++ Project Capital A/Assets/Scripts/Van Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed = 10;
-    public float turningSpeed = 60;
+    public float turningSpeed = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +15,12 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //determines left and right movement
         float horizontal = Input.GetAxis("Horizontal") * turningSpeed * Time.deltaTime;
-        transform.Rotate(0, horizontal, 0);
+        //transform.Rotate(0, horizontal, 0);
+        transform.Translate(horizontal, 0, 0);
 
+        //determines forwards and backwards movement
         float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
         transform.Translate(0, 0, vertical);
     }
