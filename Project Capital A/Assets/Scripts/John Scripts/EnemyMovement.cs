82026b48e50f1e2d187498903710f5619709db05
jdh5563 <56982973+jdh5563@users.noreply.github.com>
2020-05-13 13:05:24 -0400

    Added an enemy that follows the player

---
+++ Project Capital A/Assets/Scripts/John Scripts/EnemyMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public GameObject player;
+    private float xVel;
+    private float zVel;
+    public float turnSmoothTime;
+    private float turnSmoothVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        xVel = 0;
+        zVel = 0;
+        turnSmoothTime = 0.2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xVel = player.transform.position.x - rb.position.x;
+        zVel = player.transform.position.z - rb.position.z;
+
+        if(xVel > 2)
+        {
+            xVel = 2;
+        }
+
+        if(zVel > 2)
+        {
+            zVel = 2;
+        }
+
+        rb.velocity = new Vector3(xVel, 0, zVel);
+
+        float rotation = Mathf.Atan2(xVel, zVel) * Mathf.Rad2Deg;
+        transform.eulerAngles = new Vector3(0, rotation, 0);
+    }
+}
